661729d27c41cd385a9960db3c4a5cac957a5b48
Jordi Mas <jmas@softcatala.org>
2023-04-04 22:34:49 +0200

    Fix

---
+++ tests/testwordstosegment.py
@@ -0,0 +1,12 @@
+from src.whisper_ctranslate2.wordstosegment import WordsToSegment
+import unittest
+
+
+class TestWordsToSegment(unittest.TestCase):
+    def test_info_languages(self):
+        w = WordsToSegment()
+        self.assertEquals(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
