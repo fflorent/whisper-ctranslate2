15658c9524df32b06067465f813137f7422d76dd
Jordi Mas <jmas@softcatala.org>
2023-04-05 11:10:07 +0200

    ut

--- tests/testwordstosegment.py
+++ tests/testwordstosegment.py
@@ -4,7 +4,7 @@ import unittest
 
 
 class TestWordsToSegment(unittest.TestCase):
-    def test_check_one_line(self):
+    def test_one_segment(self):
         MAX_WORDS = 5
         words = []
         text = ""
@@ -22,6 +22,37 @@ class TestWordsToSegment(unittest.TestCase):
         self.assertEquals(10, new_segments[0].start)
         self.assertEquals(24, new_segments[0].end)
         self.assertEquals("word-0 word-1 word-2 word-3 word-4 ", new_segments[0].text)
+
+    def test_two_segments(self):
+        MAX_WORDS = 3
+        words = []
+        text = ""
+        for i in range(7):
+            word = f"word-{i} "
+            text += word
+            w = Word(10 + i, 20 + i, word, 1)
+            words.append(w)
+
+        segment = Segment(5, 30, text, words, 1, 1)
+
+        new_segments = WordsToSegment(MAX_WORDS).get_segments([segment])
+        new_segments = list(new_segments)
+
+        self.assertEquals(3, len(new_segments))
+
+        self.assertEquals(10, new_segments[0].start)
+        self.assertEquals(22, new_segments[0].end)
+        self.assertEquals("word-0 word-1 word-2 ", new_segments[0].text)
+
+        self.assertEquals(13, new_segments[1].start)
+        self.assertEquals(new_segments[1].words[0].start, new_segments[1].start)
+        self.assertEquals(25, new_segments[1].end)
+        self.assertEquals("word-0 word-1 word-2 ", new_segments[0].text)
+
+        self.assertEquals(16, new_segments[2].start)
+        self.assertEquals(new_segments[2].words[0].start, new_segments[2].start)
+        self.assertEquals(26, new_segments[2].end)
+        self.assertEquals("word-6 ", new_segments[2].text)
 
 
 if __name__ == "__main__":
